8f7c548a827fe06dfa96482c82b700a482d1115f
gfiameni <giuseppe.fiameni@gmail.com>
2016-10-26 10:29:20 +0200

    resources

--- 05-pbs.bash
+++ 05-pbs.bash
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-#PBS -l nodes=1:ppn=10,walltime=0:04:00,mem=64gb
-#PBS -N randomForest-Test
+##PBS -l nodes=1:ppn=20,walltime=0:04:00,mem=96gb
+
+#PBS -l select=1:ncpus=20:mem=96GB,walltime=0:04:00
+
 #PBS -A cin_staff
+#PBS -N Random_Forest
 #PBS -q parallel
 #PBS -V
-#PBS -e randomForest-Test.error
-#PBS -o randomForest-Test.out
+#PBS -e $PBS_JOBNAME.$PBS_JOBID.error
+#PBS -o $PBS_JOBNAME.$PBS_JOBID.out
+
+## -l select=1:ncpus=20,mem=128GB
 
 module load autoload r/3.3.1 
 
-cd $CINECA_SCRATCH/r-bench
+cd $PBS_O_WORKDIR
 
 R --vanilla < 01-randomForestR.r
+
